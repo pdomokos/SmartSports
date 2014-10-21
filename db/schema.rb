8d782a57e758b6f9af2bcd095b66ffe310050174
Balint Domokos <bdomokos74@gmail.com>
2014-10-21 09:17:26 +0200

    Add schema after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016081744) do
+ActiveRecord::Schema.define(version: 20141020084954) do
 
   create_table "connections", force: true do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141016081744) do
     t.string   "salt",             null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "connection_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
