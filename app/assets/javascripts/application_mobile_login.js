ea7ef8b199ba7337838d6a770ec6cf525debf240
Balint Domokos <bdomokos74@gmail.com>
2015-04-20 00:44:48 +0200

    Mobile login page layout fixed

---
+++ app/assets/javascripts/application_mobile_login.js
@@ -0,0 +1,23 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery-2.1.3.min
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require turbolinks
+//= require jquery.color-2.1.2.min
+//= require jquery.watermark.min
+//= require jquery.datetimepicker
+//= require d3.min
+//= require moment
+//= require_tree ./common
+//= require_tree ./mobile
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 
 Rails.application.config.assets.precompile += %w( application_mobile.css )
 Rails.application.config.assets.precompile += %w( application_mobile.js )
+Rails.application.config.assets.precompile += %w( application_mobile_login.js )
