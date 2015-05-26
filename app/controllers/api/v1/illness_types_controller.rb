687fb55b77d2cfdb8b8de4c612ba51f4a65c0804
Balint Domokos <bdomokos74@gmail.com>
2015-05-26 09:50:08 +0200

    Bugfix: api returns 400 on save/update

---
+++ app/controllers/api/v1/illness_types_controller.rb
@@ -0,0 +1,20 @@
+module Api::V1
+  class IllnessTypesController < ApplicationController
+    respond_to :json
+
+    def index
+      id = params[:id]
+      limit = params[:limit]
+      if id
+        illness_types = IllnessType.where("id = '#{id}'")
+      else
+        illness_types = IllnessType.all
+      end
+      if limit
+        illness_types = illness_types.limit(limit)
+      end
+      render json: illness_types
+    end
+
+  end
+end
--- app/controllers/save_click_record.rb
+++ app/controllers/save_click_record.rb
@@ -11,11 +11,16 @@ module SaveClickRecord
       success = true
     end
 
+    user = current_user
+    if user.nil?
+      user = current_resource_owner
+    end
+
     method = request.method.encode('utf-8')
     if method=='POST' && !request.params['_method'].nil?
       method = request.params['_method'].upcase
     end
-    clickrecord  = ClickRecord.create(user_id: current_user.id, operation_time: DateTime.now, operation: method, url: request.fullpath.encode('utf-8'), success: success, row_id: row_id, msg: msg, data: data, source: source)
+    clickrecord  = ClickRecord.create(user_id: user.id, operation_time: DateTime.now, operation: method, url: request.fullpath.encode('utf-8'), success: success, row_id: row_id, msg: msg, data: data, source: source)
     clickrecord.save!
   end
 end
