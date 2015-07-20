9ddca797fe51d71358a251a1c98cef578c4ace09
Balint Domokos <bdomokos74@gmail.com>
2015-07-20 22:20:32 +0200

    Bugfix: add auth for illness types api

--- app/controllers/api/v1/illness_types_controller.rb
+++ app/controllers/api/v1/illness_types_controller.rb
@@ -1,5 +1,7 @@
 module Api::V1
-  class IllnessTypesController < ApplicationController
+  class IllnessTypesController < ApiController
+    rescue_from Exception, :with => :general_error_handler
+    before_action :doorkeeper_authorize!, only: [:index]
     respond_to :json
 
     def index
