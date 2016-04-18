351f31b7f480cdc530f38a1925d6b4bbb8451e87
Balint Domokos <bdomokos74@gmail.com>
2016-04-18 09:10:51 +0200

    Bugfix: incorrect migration file

--- db/migrate/20160401064733_rename_medication_type_group.rb
+++ db/migrate/20160401064733_rename_medication_type_group.rb
@@ -1,3 +1,4 @@
+
 class RenameMedicationTypeGroup < ActiveRecord::Migration
   def up
     rename_column :medication_types, :group, :category
@@ -6,8 +7,8 @@ class RenameMedicationTypeGroup < ActiveRecord::Migration
   end
 
   def down
-    remove_column :medication_types, :title
     remove_index :medication_types, :title
+    remove_column :medication_types, :title
     rename_column :medication_types, :category, :group
   end
 end
